75acc65598badca8d8dd7f2aef8c87e6d873f477
Namitha Joseph <namithajosf@gmail.com>
2024-10-28 10:46:18 +0530

    arranged files

--- fastapi/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import requests
-
-app = FastAPI()
-
-@app.get("/get-go-message")
-def get_go_message():
-    response = requests.get("http://localhost:8080/message")  # Fetch message from Go server
-    return {"message": response.text}
